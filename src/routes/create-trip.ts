ea20ad0db87003f85e6154fbb723978f3b1862fc
apaolaoliveira <paolaoliveira.dev@gmail.com>
2024-08-04 04:03:22 -0300

    add participants and confirm trip, upd create trip

--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 import { prisma } from '../lib/prisma';
 import { FastifyInstance } from "fastify";
 import { getEmailClient } from '../lib/mail';
-import { ZodTypeProvider } from 'fastify-type-provider-zod';
+import { ZodTypeProvider } from 'fastify-type-provider-zod'; 
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post('/trips', {
@@ -15,24 +15,45 @@ export async function createTrip(app: FastifyInstance) {
         ends_at: z.coerce.date(),
         owner_name: z.string(),
         owner_email: z.string().email(),
+        emails_to_invite: z.array(z.string().email()),
       })
     }
   }, async (request) => {
-    const { destination, starts_at, ends_at, owner_email, owner_name } = request.body;
+    const { destination, starts_at, ends_at, owner_email, owner_name, emails_to_invite } = request.body;
 
     if(dayjs(starts_at).isBefore(new Date())) 
-      throw new Error('Invalid trip start date.')
+      throw new Error('Invalid trip start date.');
     
     if(dayjs(ends_at).isBefore(starts_at))
-      throw new Error('Invalid trip end date.')
+      throw new Error('Invalid trip end date.');
 
     const trip = await prisma.trip.create({
       data: {
         destination,
         starts_at,
         ends_at,
+        participants : {
+          createMany: {
+            data: [
+              {
+                name: owner_name,
+                email: owner_email,
+                is_confirmed: true,
+                is_owner: true
+              },
+              ...emails_to_invite.map(email => {
+                return { email }
+              })
+            ]
+          }
+        }
       }
-    })
+    });
+
+    const formattedStartDate = dayjs(starts_at).format('MM/DD/YYYY');
+    const formattedEndDate = dayjs(ends_at).format('MM/DD/YYYY');
+
+    const confirmationLink = `http://localhost:3333/trips/${trip.id}/confirm`;
 
     const mail = await getEmailClient();
 
@@ -45,12 +66,24 @@ export async function createTrip(app: FastifyInstance) {
         name: owner_name,
         address: owner_email
       },
-      subject: 'New trip scheduled',
-      text: `Hello ${owner_name},
-      A new trip has been scheduled for ${destination} from ${dayjs(starts_at).format('DD/MM/YYYY')} to ${dayjs(ends_at).format('DD/MM/YYYY')}.
-      Please confirm this trip by replying to this email.
-      Best regards,
-      Travel Planner team.`
+      subject: `Confirm your trip to ${destination} on ${formattedStartDate}`,
+      html: `
+      <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+        <p>
+          A new trip has been scheduled for <strong>${destination}</strong> from 
+          <strong>${formattedStartDate}</strong> 
+          to <strong>${formattedEndDate}</strong>.
+        </p>
+      
+        <p>Confirm this trip by the link below:</p>
+        <a href="${confirmationLink}">Confirm trip</a>
+      
+        <p>If you don't recognize this email, just ignore it.</p>
+      
+        <em>Best regards,</em><br/>
+        <strong>Travel Planner team.</strong>    
+      </div>
+      `.trim()
     })
 
     console.log(nodemailer.getTestMessageUrl(message))
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import fastify from 'fastify';
-import { createTrip } from './routes/create-trip';
+import cors from '@fastify/cors';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 
+import { createTrip } from './routes/create-trip';
+import { confirmTrip } from './routes/confirm-trip';
+
 const app = fastify();
+
+app.register(cors, {
+  origin: '*', // add frontend url here
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
 
 app.listen({ port: 3333}).then(() => {
   console.log(`Server is running on port ${3333}`);
-})
+});
